03b584707422e8279f2d1753758ed4f904e20e6c
whoisonezero <oh.shalom.0@gmail.com>
2026-03-28 23:34:07 +0900

    create new staff

--- create_staff.py
+++ create_staff.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 # Initialize Supabase
 url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_ANON_KEY")
+key: str = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
 supabase: Client = create_client(url, key)
 
 
@@ -43,14 +43,27 @@ def create_staff_member(email, password, full_name, role='usher'):
 if __name__ == "__main__":
     print("--- Church Attendance Staff Creator ---")
 
-    """ new_email = "admin.000@gmail.com"
-    new_password = "Temp@1234"
-    new_name = "Shalom"
-    new_role = "admin"  # Options: 'admin' or 'usher' """
+    new_email1 = "admin.000@gmail.com"
+    new_password1 = "chiefSt3w@rd!CCC26"
+    new_name1 = "Administrator"
+    new_role1 = "admin"
 
-    new_email = "usher.001@gmail.com"
-    new_password = "Temp@1234"
-    new_name = "Mr. Akwasi"
-    new_role = "usher"  # Options: 'admin' or 'usher'
+    new_email2 = "usher.001@gmail.com"
+    new_password2 = "1St3w@rd!CCC26"
+    new_name2 = "Usher One"
+    new_role2 = "usher"
 
-    create_staff_member(new_email, new_password, new_name, new_role)
+    new_email3 = "usher.002@gmail.com"
+    new_password3 = "2St3w@rd!CCC26"
+    new_name3 = "Usher Two"
+    new_role3 = "usher"
+
+    new_email4 = "usher.003@gmail.com"
+    new_password4 = "3St3w@rd!CCC26"
+    new_name4 = "Usher Three"
+    new_role4 = "usher"
+
+    create_staff_member(new_email1, new_password1, new_name1, new_role1)
+    create_staff_member(new_email2, new_password2, new_name2, new_role2)
+    create_staff_member(new_email3, new_password3, new_name3, new_role3)
+    create_staff_member(new_email4, new_password4, new_name4, new_role4)
